d6bc2d660786ce062b34615acbf0e65c2888e30b
MARK <MARK@DESKTOP-4GTC186>
2021-12-23 23:34:38 +0530

    This is commit change

---
+++ TestProject123/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddTwoNumbers(1, 2);
+        }
+
+        public static void AddTwoNumbers(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+    }
+}
